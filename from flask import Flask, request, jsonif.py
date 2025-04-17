6d31c3d4644d081c343e864903395ce63aece201
harshali10 <ashtekarharshali@gmail.com>
2025-04-17 11:21:03 +0530

    Delete from flask import Flask, request, jsonif.py

--- from flask import Flask, request, jsonif.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Create a FastAPI application
-app = FastAPI()
-
-# Define a simple API endpoint
-@app.get("/")
-def home():
-    return {"message": "API is running!"}
